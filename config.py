329cb7aa09e2f531b251ed28d6387e95aa74b9ec
Grzegorz Jaworski <grzegorz.jaworski@whiteaster.com>
2020-08-07 19:05:37 +0200

    Fix fernet_key default value in settings

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_name: str = os.getenv("APP_NAME", "Gitlab Helper")
     secret_key: str = os.getenv("SECRET_KEY", "default_secret_key")
-    fernet_key: bytes = bytes(os.getenv("FERNET_KEY", "default_fernet_key"), "utf-8")
+    fernet_key: bytes = bytes(os.getenv("FERNET_KEY", "cXwcpWMkuwmtsXGDJCId7eL3evbMq26pDxZMDt2tI8I="), "utf-8")
 
     access_token_algorithm: str = os.getenv("ACCESS_TOKEN_ALGORITHM", "HS256")
     access_token_expire_minutes: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 5))
